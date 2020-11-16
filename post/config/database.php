76631e7cbec4b1ccb6b59ff759c654ac0cb49e8e
Jar1ax <jar1ax.dev@gmail.com>
2020-11-16 16:25:20 +0200

    Feature test working within Sqlite memory DB

--- post/config/database.php
+++ post/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_DEFAULT', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,6 +41,13 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+//            'url' => env('DATABASE_URL'),
+            'database' => ':memory:',
+            'prefix' => '',
+//            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
