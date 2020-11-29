478369402ecf6a65723c3e5488b0b479a8c49d3e
jar1ax <jar1ax.dev@gmail.com>
2020-11-29 17:30:12 +0200

    CSS in PDF a bit improved

--- post/resources/views/pdf/delete.blade.php
+++ post/resources/views/pdf/delete.blade.php
@@ -4,21 +4,20 @@
     .h1{
         font-family: Consolas, monospace;
         alignment: center;
+        color: #00baff;
         text-align: center;
     }
     .kek{
-        background: darkgray;
-        alignment: center;
 
+        background: rgb(2,0,36);
     }
     .img{
-        alignment: center;
-        width: 200px;
+        width: 400px;
         height: auto;
     }
     .p{
         font-family: FreeSerif, cursive;
-        font-size: medium;
+        font-size: large;
         color: dodgerblue;
         text-align: center;
     }
@@ -32,7 +31,7 @@
             <br/>
             <br/>
             <br/>
-            <img class="img" src="https://happyologist.co.uk/wp-content/uploads/hugs.jpg">
+            <p class="p"><img class="img" src="https://happyologist.co.uk/wp-content/uploads/hugs.jpg"></p>
             <br/>
              <p class="p">In case you would change your mind, back to email and click on button. </p>
     </body>
